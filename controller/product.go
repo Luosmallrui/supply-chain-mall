eeed4f03e932fd7566440033bb7b46473e28f111
luosmallrui <luoxiaorui115815@qq.com>
2025-08-11 23:52:10 +0800

    update

--- controller/product.go
+++ controller/product.go
@@ -1,10 +1,14 @@
 package controller
 
 import (
+	"encoding/json"
+	"fmt"
 	"github.com/gin-gonic/gin"
+	"io/ioutil"
 	"supply-chain-mall/pkg/ctx"
 	"supply-chain-mall/svc"
 	"supply-chain-mall/types"
+	"time"
 )
 
 type Product struct {
@@ -15,8 +19,61 @@ func (p *Product) RegisterRouter(r gin.IRouter) {
 	product := r.Group("/product")
 	product.GET("", ctx.HandlerFunc(p.FindAllProducts))
 	product.POST("", ctx.HandlerFunc(p.CreateProduct))
-
+	g := r.Group("/fruit")
+	g.GET("/list", GetFruits)
+	g.GET("/details", GetFruitsDetail)
 }
+
+func GetFruits(c *gin.Context) {
+	filePath := "a.json"
+
+	// 读取文件
+	jsonBytes, err := ioutil.ReadFile(filePath)
+	if err != nil {
+		fmt.Println("读取文件失败:", err)
+		return
+	}
+
+	// 定义变量接收解析结果
+	var productList types.ProductList
+
+	// 解析 JSON
+	err = json.Unmarshal(jsonBytes, &productList)
+	if err != nil {
+		fmt.Println("解析 JSON 失败:", err)
+		return
+	}
+
+	// 使用解析后的数据
+	fmt.Printf("共解析到 %d 个产品\n", len(productList.Products))
+	for _, p := range productList.Products {
+		fmt.Printf("产品 ID: %s 名称: %s 创建时间: %s\n", p.ID, p.Name, p.CreatedAt.Format(time.RFC3339))
+	}
+	c.JSON(200, gin.H{"data": productList})
+}
+
+func GetFruitsDetail(c *gin.Context) {
+	filePath := "b.json"
+
+	// 读取文件
+	jsonBytes, err := ioutil.ReadFile(filePath)
+	if err != nil {
+		fmt.Println("读取文件失败:", err)
+		return
+	}
+
+	// 定义变量接收解析结果
+	var productList types.Fruit
+
+	// 解析 JSON
+	err = json.Unmarshal(jsonBytes, &productList)
+	if err != nil {
+		fmt.Println("解析 JSON 失败:", err)
+		return
+	}
+	c.JSON(200, gin.H{"data": productList})
+}
+
 func (p *Product) FindAllProducts(c *gin.Context) error {
 	resp, err := p.ProductSvc.FindAllProduct(c)
 	if err != nil {
--- types/product.go
+++ types/product.go
@@ -1,5 +1,7 @@
 package types
 
+import "time"
+
 type Product struct {
 	ID            int     `json:"id"`
 	Name          string  `json:"name"`
@@ -8,4 +10,61 @@ type Product struct {
 	Image         string  `json:"image"`
 	Brand         string  `json:"brand"`
 	BrandSubtitle string  `json:"brandSubtitle"`
+}
+type ProductList struct {
+	Products []Fruit `json:"products"`
+}
+
+type Fruit struct {
+	ID             string    `json:"id"`
+	Gender         []string  `json:"gender"`
+	Images         []string  `json:"images"`
+	Reviews        []Review  `json:"reviews"`
+	Publish        string    `json:"publish"`
+	Ratings        []Rating  `json:"ratings"`
+	Category       string    `json:"category"`
+	Available      int       `json:"available"`
+	PriceSale      float64   `json:"priceSale"`
+	Taxes          float64   `json:"taxes"`
+	Quantity       int       `json:"quantity"`
+	InventoryType  string    `json:"inventoryType"`
+	Tags           []string  `json:"tags"`
+	Code           string    `json:"code"`
+	Description    string    `json:"description"`
+	SKU            string    `json:"sku"`
+	CreatedAt      time.Time `json:"createdAt"`
+	Name           string    `json:"name"`
+	Price          float64   `json:"price"`
+	CoverURL       string    `json:"coverUrl"`
+	Colors         []string  `json:"colors"`
+	TotalRatings   float64   `json:"totalRatings"`
+	TotalSold      int       `json:"totalSold"`
+	TotalReviews   int       `json:"totalReviews"`
+	NewLabel       Label     `json:"newLabel"`
+	SaleLabel      Label     `json:"saleLabel"`
+	Sizes          string    `json:"sizes"`
+	SubDescription string    `json:"subDescription"`
+}
+
+type Review struct {
+	ID          string    `json:"id"`
+	Name        string    `json:"name"`
+	PostedAt    time.Time `json:"postedAt"`
+	Comment     string    `json:"comment"`
+	IsPurchased bool      `json:"isPurchased"`
+	Rating      float64   `json:"rating"`
+	AvatarURL   string    `json:"avatarUrl"`
+	Helpful     int       `json:"helpful"`
+	Attachments []string  `json:"attachments"`
+}
+
+type Rating struct {
+	Name        string `json:"name"`
+	StarCount   int    `json:"starCount"`
+	ReviewCount int    `json:"reviewCount"`
+}
+
+type Label struct {
+	Enabled bool   `json:"enabled"`
+	Content string `json:"content"`
 }
